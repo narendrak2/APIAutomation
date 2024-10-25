421614371994269367f75ff9ed61b717071530b1
narendra12345 <narendra.kulkarnichmkd@gmail.com>
2024-10-25 13:02:05 +0530

    changes done on extent reports

--- src/main/java/Reporting/Setup.java
+++ src/main/java/Reporting/Setup.java
@@ -1,3 +1,4 @@
+
 package Reporting;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -8,28 +9,34 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.File;
+
 public class Setup implements ITestListener {
 
     public static ExtentReports report;
+    public static String fullreportpath;
     public static ThreadLocal<ExtentTest> ExtentTest = new ThreadLocal<>();
 
     public void onStart(ITestContext context) {
 
         String fileName = Extentreportmanager.getReportNameWithTimeStamp();
-        String fullreportpath = System.getProperty("user.dir") + "\\reports\\" + fileName;
+        fullreportpath = System.getProperty("user.dir") + "//reports//" + fileName;
+        File reports=new File(fullreportpath);
+        if(reports.exists()){
+            reports.delete();
+        }
         report = Extentreportmanager.createinstance(fullreportpath, "API automation report", "test excution report");
-
     }
 
     public void onFinish(ITestContext context) {
-        if (report != null)
+        if(report!=null){
             report.flush();
+        }
     }
 
     public void onTestStart(ITestResult result) {
 
-        ExtentTest test = report.createTest("Test name=" + result.getMethod().getMethodName());
+        ExtentTest test= report.createTest("Test name=" + result.getMethod().getMethodName());
         ExtentTest.set(test);
     }
-
 }
--- src/main/java/RestUtil/RestUtils.java
+++ src/main/java/RestUtil/RestUtils.java
@@ -37,22 +37,25 @@ public class RestUtils {
                 .extract().
                 response();
     }
-
-
     private static void PrintrequestloginReport(RequestSpecification requestSpecification) {
 
         QueryableRequestSpecification queryableRequestSpecification = SpecificationQuerier.query(requestSpecification);
         Extentreportmanager.loginfoDetails("End point is " + queryableRequestSpecification.getBaseUri());
         Extentreportmanager.loginfoDetails("Method name is " + queryableRequestSpecification.getMethod());
+        Extentreportmanager.loginfoDetails("Method name is " + queryableRequestSpecification.getHeaders().asList().toString());
+        Extentreportmanager.loginfoDetails("request body is ");
+        Extentreportmanager.logjosn(queryableRequestSpecification.getBody());
     }
-
     private static void printLogInResponseReport(Response response) {
 
+        //System.out.println("Resposne status code is " + response.getStatusCode());
+        //System.out.println("Response headers are " + response.headers());
+        //System.out.println("Response headers are " + response.getBody().prettyPrint());
         Extentreportmanager.loginfoDetails("Resposne status code is " + response.getStatusCode());
-        Extentreportmanager.loginfoDetails("Response headers are " + response.headers());
-        Extentreportmanager.loginfoDetails("Response headers are " + response.body());
+        Extentreportmanager.loginfoDetails("Response headers are " + response.headers().asList().toString());
+        Extentreportmanager.loginfoDetails("Response body is");
+        Extentreportmanager.logjosn(response.getBody().prettyPrint());
     }
-
     //public static Response performPost(String endPoint, String createairlinePayload, HashMap<String, Object> headers) {
 
     //return RestAssured.given().log().all().baseUri(endPoint).headers(headers)
